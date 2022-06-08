d23334abd29c6f2a83935032fca23646f91e6d24
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-07 19:52:20 -0500

    created the nav bar and welcome page and installed axios and react router

--- project2_fe_frittte/src/App.js
+++ project2_fe_frittte/src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+import Welcome from "./Components/WelcomePage";
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Welcome />
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ project2_fe_frittte/src/Components/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import MainNavBar from "./MainNavBar";
+
+export default function Welcome(){
+    return(
+        <>
+         <MainNavBar />
+            <center>
+                <h1>FRITTTE</h1>
+            </center>
+        </>
+    )
+}
