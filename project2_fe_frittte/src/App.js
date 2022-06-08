3eceee48038b553f1f0ecae5d9eb4c034fe2be8d
Asrat Minase <asrat461@revature.net>
2022-06-08 00:17:34 -0500

    Updated and added customer component which consists of add/update/delete an account, customer-dashboard and customerNavbar

--- project2_fe_frittte/src/App.js
+++ project2_fe_frittte/src/App.js
@@ -3,9 +3,11 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AdminDashboard from "./admin/admin-dashboard";
-
-
-import WelcomePage from "./Components/WelcomePage";
+import AccountRegister from "./customers/create-an-account";
+import CustomerDashboard from "./customers/customer-dashboard";
+import AccountDelete from "./customers/delete-an-account";
+import AccountUpdate from "./customers/update-an-account";
+import Welcome from "./WelcomePage";
 
 
 function App() {
@@ -16,7 +18,11 @@ function App() {
           {/* <WelcomePage /> */}
               <Routes>
                  <Route path="/admindashboard" element={<AdminDashboard />} />
-                 <Route path="" element={<WelcomePage />} />
+                 <Route path="" element={<Welcome />} />
+                 <Route path="/customerdashboard" element={<CustomerDashboard/>} />
+                 <Route path="/addanaccount" element={<AccountRegister/>}/>
+                 <Route path="/updateanaccount" element={<AccountUpdate/>}/>
+                 <Route path="/deleteanaccount" element={<AccountDelete/>}/>
               
                </Routes> 
       </BrowserRouter>
