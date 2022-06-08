4ca13160fc2b7bae8f3de54a75dd6a7d9fa97c1f
Ay Sebirka <ay914@revature.net>
2022-06-07 21:05:27 -0500

    creating function and adding routes

--- project2_fe_frittte/src/App.js
+++ project2_fe_frittte/src/App.js
@@ -1,8 +1,31 @@
+import UpdateProduct from "./admin/update-product";
+import AdminDashboard from "./admin/admin-dashboard";
+import AddProduct from "./admin/add-product";
+import DeleteProduct from "./admin/delete-product";
+import NavBar from "./components/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
     <>
-    </>
+     <BrowserRouter>
+                
+                
+                    <NavBar />
+                    <Routes>
+                       <Route path="admindashboard" element={<AdminDashboard></AdminDashboard>} />
+                       <Route path="addproduct" element={<AddProduct></AddProduct>} />
+                       <Route path="updateproduct" element={<UpdateProduct></UpdateProduct>} />
+                       <Route path="deleteproduct" element={<DeleteProduct></DeleteProduct>} />
+ 
+                     </Routes>
+                
+      </BrowserRouter>
+
+     </>
+   
   );
 }
 
