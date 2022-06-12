ee3d71bbc90893aac55b6a316d8a3e0e4f7ca845
Geoffrey <bloisgeoffrey@yahoo.com>
2022-06-12 16:00:32 -0500

    working on the login for customer

--- project2_fe_frittte/src/components/customers/CustomerLogin.jsx
+++ project2_fe_frittte/src/components/customers/CustomerLogin.jsx
@@ -1,24 +1,35 @@
-import { Input } from "@mui/material";
+import { Input ,Button } from "@mui/material";
 import axios from "axios";
 import { useRef } from "react";
-import { Button } from "react-chat-engine";
+import { useNavigate } from "react-router-dom";
 
 export default function CustomerLogin(){
     
+    const navigate = useNavigate();
+
     const usernameInput = useRef();
     const passwordInput = useRef();
-    const url = "http://localhost:9006/customer";
+    const url = "http://localhost:9006";
     
     async function Login(){
         
         const user = {
-        username : usernameInput.current.value,
-        password  : passwordInput.current.value   
-        }
-
+        username:usernameInput.current.value,
+        password:passwordInput.current.value,   
+        };
+        console.log(user)
         try{
-            const response = axios.post(`${url}/login`, user)
+            console.log(user)
+            const response = axios.post(`${url}/auth`, user)
             console.log(response.data)
+            
+        if(usernameInput.current.value === "ASebirka" || usernameInput.current.value === "mmark" ){
+            
+              navigate("/admindashboard");
+            } else{
+                navigate("/customerdashboard");
+               
+            }
         } catch(error){
             console.log(error)
         }
@@ -32,8 +43,10 @@ export default function CustomerLogin(){
         <>
         <h3>Please Login Below</h3>
         <br></br>
-        <Input placeholder="Enter your Username" ref={usernameInput}></Input>
-        <Input placeholder="Enter your Password" ref={passwordInput}></Input>
+        <input placeholder="Enter your Username" ref={usernameInput}></input>
+        <input placeholder="Enter your Password" ref={passwordInput}></input>
+         
+        <br></br>
         <Button onClick={Login}>Log In</Button>
         
         
