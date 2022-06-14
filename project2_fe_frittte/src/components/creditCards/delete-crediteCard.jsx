5c72a117cd944203d52f32e195c40b212eb3d37b
Asrat Minase <asrat461@revature.net>
2022-06-14 15:29:08 -0500

    updated the credit card /delete

--- project2_fe_frittte/src/components/creditCards/delete-crediteCard.jsx
+++ project2_fe_frittte/src/components/creditCards/delete-crediteCard.jsx
@@ -8,40 +8,41 @@ export default function CrdeiteCardRegister() {
 
     const [user, setUser] = useContext(userContext);
 
-    const creditCardInput = useRef();
-    // const creditCradNameInput  = useRef();
-    // const cvvInput = useRef();
-    // const expDateInput = useRef();
-    // const limitInput = useRef();
-    // const usernameInput = useRef();
+    const creditCardInput = useRef()
 
     async function deleteCC() {
-    console.log(user.username)
-
-
+    // console.log(creditCardInput.current.value)
+    // console.log(user.username)
+    
+    
         const userCC = {
 
             creditCardNumber: creditCardInput.current.value,
-            // creditCardName: creditCradNameInput.current.value,
-            // cvv: cvvInput.current.value,
-            // expDate: expDateInput.current.value,
-            // limit: limitInput.current.value,
             customerUsername: user.username,  
     };
 
         try {
-                          
-            const response = await axios.delete(`${url}//deleteCreditCard`, userCC, {withCredentials: true});
+            const responseCheck = await axios.get(`${url}/findCard?findCreditCard=${creditCardInput.current.value}`)
+            const checkCard = creditCardInput.current.value
+            console.log(responseCheck.data.creditCardNumber)
+            console.log(checkCard)
+
+            if(checkCard === responseCheck.data.creditCardNumber){
+                const response = await axios.delete(`${url}/delete?creditCardNumber=${creditCardInput.current.value}`, userCC, {withCredentials: true})
+            }else if(responseCheck.data.creditCardNumber===null){
+                alert("Credit Card is already deleted")
+                console.log(responseCheck)
+            }
             
         } catch (error) {
-            alert(error.response.data);
-           
+            
+            alert(creditCardInput.current.value);
         }
     }
 
     return (
         <>
-                <h4>Please register below.</h4>
+                <h4>You can delete your Credit Card here.</h4>
                 <input type = "credit card" placeholder="Enter credit card number" ref={creditCardInput}></input>
                 {/* <input placeholder="Enter name on the credit card number" ref={creditCradNameInput}></input>
                 <br></br>
